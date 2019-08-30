fa306b831a5bbba14bc2373f67ddf157f8e1eb7b
John <admin@tests-MBP-3.fios-router.home>
2019-08-30 14:36:53 -0400

    Added progress indicator.

--- upload-dropbox.py
+++ upload-dropbox.py
@@ -3,9 +3,15 @@ import urllib.request
 from datetime import timedelta, date
 import pytz
 import configparser
+from math import floor
 from boto_uploader import BotoUploader
 from dropbox_uploader import DropboxUploader
 from ring_camera import RingCamera
+
+
+def floored_percentage(val, digits):
+    val *= 10 ** (digits + 2)
+    return '{1:.{0}f}%'.format(digits, floor(val) / 10 ** digits)
 
 
 def process_videos():
@@ -20,6 +26,7 @@ def process_videos():
     yesterday = date.today() - timedelta(days=1)
     est = pytz.timezone(config['Ring']['timezone'])
 
+    print("Retrieving videos for " + yesterday.strftime("%m/%d/%Y") + "...")
     videos = ring_camera.get_motion_videos_by_date(yesterday, est)
 
     for video in videos:
@@ -30,8 +37,10 @@ def process_videos():
             dropbox_uploader.upload_file(video.filename, video.filepath)
 
         os.remove(video.filename)
-        print(video.filepath)
+        progress = videos.index(video) / len(videos)
+        print(floored_percentage(progress, 2) + " processed")
 
 
 if __name__ == '__main__':
     process_videos()
+    print("All videos processed")
